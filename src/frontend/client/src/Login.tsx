282a968da5e9bc3b88b50f869c76a480984deae7
white-calx <77462841+white-calx@users.noreply.github.com>
2024-10-15 21:59:32 -0400

    missing types for express setting up basic outline for login page (dashboard to come later)

---
+++ src/frontend/client/src/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+
+function Login(){
+    return(
+        <div className="Login">
+            <header className="Login-header">
+                <pre className="Login-head-text">
+                    Login placeholder page <br/>
+                    This is just a placeholer page <br/>
+                </pre>
+            </header>
+        </div>
+    )
+}
+
+export default Login;
