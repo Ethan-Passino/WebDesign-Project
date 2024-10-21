d2adc276c0669acde0b075a6fe8eb007471fcd6e
Ethan Passino <itzethanpassino@gmail.com>
2024-10-20 22:05:04 -0400

    redo everything so it all works with google cloud. This should be the last time we redo anything!

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const db = require('mongodb');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('webdesign/dist'));
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+    console.log("base page");
+});
