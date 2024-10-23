3c80c4f3bfd8179a9367bf2cd9d9f2c65f2fb9bb
white-calx <77462841+white-calx@users.noreply.github.com>
2024-10-23 08:21:17 -0400

    minor visibility improvements to Dashboard.tsx & TaskList.tsx

--- webdesign/src/Dashboard/Dashboard.tsx
+++ webdesign/src/Dashboard/Dashboard.tsx
@@ -1,85 +1,51 @@
 import "./Dashboard.css";
+
 import {TaskList} from './Dashboard Components/TaskList'
 
-/*
-interface taskItem{
-    id: number;
-    text: string;
-}
-    */
+//TODO: Gamification bar
 
 export function Dashboard() {
-    /*
-    const [tasks, setTasks]  = useState<taskItem[]>([]);
-    const [newTaskText, setNewTaskText] = useState<string>('');
-
-    const addTask = () => {
-        if(newTaskText.trim() !== ''){
-            const newTask: taskItem = {id: Date.now(), text: newTaskText};
-            setTasks([...tasks, newTask]);
-            setNewTaskText('');
-
-        }
-    }
-
-
-    const updateTask = (id: number, newText: string)=> {
-        const updatedTask = tasks.map(task =>
-            task.id === id ? {...task, text: newText} : task);
-        setTasks(updatedTask);
-
-    };
-*/
     return (
         <div className="Dashboard-root">
             <h1 className={"Root-header"}>This is an attempt at at a dashboard</h1>
             <div className={"Layout-grid"}>
                 <div className={"Task-root task1"}>
-                    <h1 className={"Board-header"}>Backlog</h1>
+                    <h3 className={"Board-header"}>Backlog</h3>
                     <div className={"Task-list-root"}>
                         <TaskList></TaskList>
                     </div>
-                    <textarea className={"Task-text"}>
-                        This is a paragraph
-                    </textarea>
+
                     <div className={"Game-root"}>
-                        <h1 className={"Game-header"}>Gameification area</h1>
+                        <h4 className={"Game-header"}>Gamification area</h4>
                     </div>
                 </div>
                 <div className={"Task-root task2"}>
-                    <h1 className={"Board-header"}>To-do</h1>
+                    <h3 className={"Board-header"}>To-do</h3>
                     <div className={"Task-list-root"}>
                         <TaskList></TaskList>
                     </div>
-                    <textarea className={"Task-text"} rows={3} cols={10}>
-                        This is a paragraph
-                    </textarea>
                     <div className={"Game-root"}>
-                        <h1 className={"Game-header"}>Gameification area</h1>
+                        <h4 className={"Game-header"}>Gamification area</h4>
                     </div>
                 </div>
                 <div className={"Task-root task3"}>
-                    <h1 className={"Board-header"}>In Progress</h1>
+                    <h3 className={"Board-header"}>In Progress</h3>
                     <div className={"Task-list-root"}>
                         <TaskList></TaskList>
                     </div>
-                    <textarea className={"Task-text"}>
-                        This is a paragraph
-                    </textarea>
+
                     <div className={"Game-root"}>
-                        <h1 className={"Game-header"}>Gameification area</h1>
+                        <h4 className={"Game-header"}>Gamification area</h4>
                     </div>
                 </div>
                 <div className={"Task-root 4"}>
-                    <h1 className={"Board-header"}>Backlog</h1>
+                    <h3 className={"Board-header"}>Backlog</h3>
                     <div className={"Task-list-root"}>
                         <TaskList></TaskList>
                     </div>
-                    <textarea className={"Task-text"}>
-                            This is a paragraph
-                        </textarea>
+
                     <div className={"Game-root"}>
-                        <h1 className={"Game-header"}>Gameification area</h1>
+                        <h4 className={"Game-header"}>Gamification area</h4>
                     </div>
                 </div>
             </div>
