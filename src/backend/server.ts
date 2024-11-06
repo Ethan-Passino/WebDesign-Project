20079e31a598b8ff7b6a973fcfa9b095f0eafd69
Ethan Passino <itzethanpassino@gmail.com>
2024-11-06 15:02:45 -0500

    Config changes and backend changes for google cloud support.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, '../../src/frontend/build')));
-
 // Handle any requests that don't match the API routes
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../src/frontend/build', 'index.html'));
